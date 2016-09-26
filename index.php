4aca3f81b2f4e32a36d0892759821a4f115be8e7
djj128 <djj128@qq.com>
2016-09-26 08:13:18 +0800

    add user page

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-   echo 'hello,myGit';
+   header("Content-type:text/html;charset=utf-8");
+   echo 'hello,myGit<br />';
+   echo 'commit git ok';
+
+   $the_hour = date('G');
+
+    if($the_hour <=8){
+        echo '<br> 早上好';
+    }elseif($the_hour>8 && $the_hour<=12){
+        echo '上午好';
+    }elseif ($the_hour >12 && $the_hour<=18) {
+         echo '下午好';
+     }
+
  ?>
